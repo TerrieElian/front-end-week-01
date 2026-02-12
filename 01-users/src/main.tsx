a34a8da482f3f5ad1fce80f2a7ba22b1e280dc9a
TerrieElian <elian.terrie@student.howest.be>
2026-02-12 14:53:00 +0100

    Refactor code structure for improved readability and maintainability

--- 01-users/src/main.tsx
+++ 01-users/src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="bg-pink-500">
+      <App />
+    </div>
   </StrictMode>,
 )
